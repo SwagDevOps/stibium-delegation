a09f123cb720ea8ff4ebab1ab763fa81436f3f96
Dimitri Arrigoni <dimitri@arrigoni.me>
2020-05-16 15:19:48 +0200

    delegation (spec) major changes and improvements
    * constants resolution examples added
    * respond_to example only for not private|protected methods
    * cs update
    * sham!(:utils).mktemp use
    * failing example on protected method inspection

--- spec/stibium/delegation_spec.rb
+++ spec/stibium/delegation_spec.rb
@@ -1,8 +1,21 @@
 # frozen_string_literal: true
 
+# constants ---------------------------------------------------------
+describe Stibium::Delegation, :'stibium/delegation' do
+  [ # @formatter:off
+    :Errors,
+    :Inspection,
+    :Methodifier,
+    :VERSION,
+  ].each do |const_name| # @formatter:on
+    it { expect(described_class).to be_const_defined(const_name) }
+  end
+end
+
+# samples -----------------------------------------------------------
 sham!(:samples).results.each do |sample_name, results|
   sham!(:samples).instances.fetch(sample_name).tap do |instance|
-    results.each_key do |method_name|
+    results.to_a.reject { |v| v.fetch(1) =~ /^(protected|private)\s/ }.to_h.each_key do |method_name|
       describe instance.class, :'stibium/delegation', :methods do
         let(:subject) { instance }
 
@@ -16,9 +29,7 @@ sham!(:samples).results.each do |sample_name, results|
           let(:described_class) { instance.class }
           let(:code) { method_code.chomp }
 
-          # rubocop:disable Layout/LineLength
           it { expect(described_class.delegation[method_name]&.chomp).to eq(code) }
-          # rubocop:enable Layout/LineLength
         end
       end
     end
@@ -26,18 +37,36 @@ sham!(:samples).results.each do |sample_name, results|
 end
 
 # testing real behavior ---------------------------------------------
-
-autoload(:SecureRandom, 'securerandom')
-
 sham!(:samples).instances.fetch(:fileutils_touch).tap do |sample|
   describe sample.class, :'stibium/delegation' do
     let(:subject) { sample }
 
     it { expect(subject).to respond_to(:touch) }
 
-    tmpdir.join("rspec_#{SecureRandom.hex}").tap do |file|
+    sham!(:utils).mktemp.call(path: tmpdir, verbose: true, dry_run: true).tap do |file|
       context "#touch(#{file.inspect})" do
         it { expect(subject.touch(file)).to eq([file.to_s]) }
+      end
+    end
+  end
+end
+
+# failure with inspection on protected method -----------------------
+sham!(:samples).instances.fetch(:empty).tap do |sample|
+  describe sample.class, :'stibium/delegation' do
+    let(:described_class) { sample.class }
+    let(:delegation_type) { described_class.__send__(:status_class) }
+
+    context '.delegate(:failed?, to: :"@status") { delegation_type } ' do
+      [ # @formatter:off
+        RuntimeError,
+        Stibium::Delegation::Errors::NoPublicMethodError,
+      ].each do |error_class| # @formatter:on
+        it do
+          expect do
+            described_class.__send__(:delegate, :failed?, to: :'@status', & -> { delegation_type })
+          end.to raise_error(error_class, /method `failed\?' for #<Class:.*> is not a public method/)
+        end
       end
     end
   end
