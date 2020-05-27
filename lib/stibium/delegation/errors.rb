0d8c00896bd4da312e26c9d9cc2efe6ed76efa61
Dimitri Arrigoni <dimitri@arrigoni.me>
2020-05-27 18:01:15 +0200

    errors (lib) refactor + add new errors

--- lib/stibium/delegation/errors.rb
+++ lib/stibium/delegation/errors.rb
@@ -10,34 +10,59 @@ require_relative '../delegation'
 
 # Errors module
 module Stibium::Delegation::Errors
+  # @abstract
+  class Error < ::RuntimeError
+  end
+
   # Parameter inspection error
-  class UnsupportedParameterTypeError < RuntimeError
+  class UnsupportedParameterTypeError < Error
     # @type [Symbol|String]
     attr_reader :type
 
     # @param [Symbol|String] type
     def initialize(type:)
       @type = type.to_sym
+    end
 
-      super("Unsupported type #{type.inspect}")
+    def to_s
+      "Unsupported type #{type.inspect}"
     end
   end
 
-  # Method inspection error
-  class NoPublicMethodError < RuntimeError
+  # @abstract
+  class MiisingSymbolError < Error
     # @type [Class|Module|Object]
     attr_reader :type
 
     # @type [Symbol]
-    attr_reader :method
+    attr_reader :name
 
     # @param [Class|Module|Object] type
     # @param [Symbol|String] method
-    def initialize(type:, method:)
+    def initialize(type:, name:)
       @type = type
-      @method = method.to_sym
+      @name = name.to_sym
+    end
+  end
 
-      super("method `#{method}' for #{type} is not a public method")
+  # Method reflection error
+  class NoConstantError < MiisingSymbolError
+    def to_s
+      "uninitialized constant #{name} for #{type}"
+    end
+  end
+
+  # Method reflection error
+  class NoMethodError < Error
+    def to_s
+      "undefined method `#{name}' for #{type}"
+    end
+  end
+
+  # Method inspection error
+  class NoPublicMethodError < MiisingSymbolError
+    def to_s
+      "method `#{name}' for #{type} is not a public instance method"
     end
   end
 end
